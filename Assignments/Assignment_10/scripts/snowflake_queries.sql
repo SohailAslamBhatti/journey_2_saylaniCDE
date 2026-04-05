7f61cce9e7871eafe2b196d022dfc4b72fba4c3f
Sohail Aslam Bhatti <sohailaslambhatti@outlook.com>
2026-04-06 01:02:27 +0500

    Update snowflake_queries.sql

--- Assignments/Assignment_10/scripts/snowflake_queries.sql
+++ Assignments/Assignment_10/scripts/snowflake_queries.sql
@@ -1,1 +1,14 @@
+-- Create Table
+DROP TABLE IF EXISTS city_info;
 
+CREATE TABLE IF NOT EXISTS city_info(
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    census_2020 NUMERIC NOT NULL,
+    land_area_sq_mile_2020 NUMERIC NOT NULL
+);
+
+-- Copy Data
+COPY INTO city_database.new_city_schema.city_info
+FROM @city_database.new_city_schema.snowflake_ext_stage_yml
+FILE_FORMAT = csv_format;
